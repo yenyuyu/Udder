85ab835a0e82856bb091910efe1953f58293c011
Yu Yen <nuageyen@gmail.com>
2020-03-08 18:50:21 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by RainbowDash on 2020/3/8.
+//  Copyright © 2020 YunYen Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
